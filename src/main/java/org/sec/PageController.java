e5f0398e16f74cdd398d1112a0149e27cc4fd36b
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-04 19:12:15 -0300

    init pages

---
+++ src/main/java/org/sec/PageController.java
@@ -0,0 +1,22 @@
+package org.sec;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping({"/welcome", "/home"})
+    public String handleWelcome() {
+        return "welcome";
+    }
+
+    @GetMapping("/admin/home")
+    public String handleAdminHome() {
+        return "admin_home";
+    }
+
+    @GetMapping("/user/home")
+    public String handleUserHome() {
+        return "user_home";
+    }
+}
