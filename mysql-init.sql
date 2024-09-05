353da931df794a90abd0fc88283f01e3f6ea76a5
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-05 17:44:25 -0300

    mysql compose

--- mysql-init.sql
+++ mysql-init.sql
@@ -1,1 +1,3 @@
-ALTER USER 'admin'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'pass';
+CREATE USER 'admin'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'pass';
+GRANT ALL PRIVILEGES ON sec.* TO 'admin'@'%';
+FLUSH PRIVILEGES;
